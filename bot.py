8e7719b8bacae4243391e6119a9107dd0e33833b
yntymakdev <kubanychevyntymak21@gmail.com>
2025-04-10 23:03:54 +0600

    new com

--- bot.py
+++ bot.py
@@ -8,51 +8,40 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+import os
 
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = getenv("7953693443:AAHuDE6tAfCNfN2ACD-C_oi3I03hNbBp-PE")
+# Загружаем токен из переменной окружения
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-# All handlers should be attached to the Router (or Dispatcher)
+# Настроим логирование
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-dp = Dispatcher()
-
+# Инициализируем бота и диспетчер
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(bot)
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
     """
     Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
-
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
+    # Start polling to handle events
+    await dp.start_polling()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # Запуск основного процесса
     asyncio.run(main())
